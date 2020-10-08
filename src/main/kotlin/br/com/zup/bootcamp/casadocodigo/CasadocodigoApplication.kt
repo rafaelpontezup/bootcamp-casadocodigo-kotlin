ccc04576ed9f99066ad73e076d4a1da0fd648f76
Rafael Ponte <rafael.ponte@zup.com.br>
2020-10-08 20:48:04 -0300

    first commit with:    - Java 11, Maven and Kotlin;    - Spring Boot modules:        * Spring Web;        * Spring Data Jpa (and PostgreSQL driver);        * Spring Validation;        * Spring DevTools;    - Docker Compose for PostgreSQL;

---
+++ src/main/kotlin/br/com/zup/bootcamp/casadocodigo/CasadocodigoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.bootcamp.casadocodigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CasadocodigoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CasadocodigoApplication>(*args)
+}
