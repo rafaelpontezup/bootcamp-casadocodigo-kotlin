2ad13aa628b5b22b50f8b28ba30584808afc102b
Rafael Ponte <rafael.ponte@zup.com.br>
2020-10-08 20:51:23 -0300

    adiciona feature para cadastrar novo autor;

---
+++ src/main/kotlin/br/com/zup/bootcamp/casadocodigo/controller/AutorController.kt
@@ -0,0 +1,28 @@
+package br.com.zup.bootcamp.casadocodigo.controller
+
+import br.com.zup.bootcamp.casadocodigo.model.Autor
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+import javax.persistence.EntityManager
+import javax.transaction.Transactional
+import javax.validation.Valid
+
+@RestController
+class AutorController(private val manager: EntityManager) {
+
+    @Transactional
+    @PostMapping("/api/autores")
+    fun novo(@Valid novo: AutorRequest, uriBuilder: UriComponentsBuilder): ResponseEntity<Any> {
+
+        val autor: Autor = novo.toModel()
+        manager.persist(autor)
+
+        val location: URI = uriBuilder.path("/api/autores/{id}").buildAndExpand(autor.id).toUri()
+        return ResponseEntity
+                .created(location).build<Any>()
+    }
+
+}
---
+++ src/main/kotlin/br/com/zup/bootcamp/casadocodigo/model/Autor.kt
@@ -0,0 +1,23 @@
+package br.com.zup.bootcamp.casadocodigo.model
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+class Autor(
+        @Id
+        @GeneratedValue
+        var id: Long? = null,
+        @Column(nullable = false)
+        var nome: String,
+        @Column(nullable = false)
+        var email: String,
+        @Column(length = 400, nullable = false)
+        var descricao: String,
+        @Column(nullable = false)
+        @Temporal(TemporalType.TIMESTAMP)
+        var criadoEm: LocalDateTime = LocalDateTime.now()
+) {
+
+
+}
