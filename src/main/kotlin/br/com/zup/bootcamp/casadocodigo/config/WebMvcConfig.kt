5e38566dc3eb206f6cacc39f7b627c317b1763df
Rafael Ponte <rafael.ponte@zup.com.br>
2020-10-10 00:26:32 -0300

    configura locale para pt_BR;

---
+++ src/main/kotlin/br/com/zup/bootcamp/casadocodigo/config/WebMvcConfig.kt
@@ -0,0 +1,37 @@
+package br.com.zup.bootcamp.casadocodigo.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.i18n.CookieLocaleResolver
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
+import java.util.*
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    companion object {
+        private val PT_BR = Locale("pt", "BR")
+    }
+
+    @Bean
+    fun localeResolver(): LocaleResolver {
+        val resolver = CookieLocaleResolver()
+        resolver.setDefaultLocale(PT_BR)
+        return resolver
+    }
+
+    @Bean
+    fun localeChangeInterceptor(): LocaleChangeInterceptor {
+        val interceptor = LocaleChangeInterceptor()
+        interceptor.paramName = "lang"
+        return interceptor
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(localeChangeInterceptor())
+    }
+
+}
